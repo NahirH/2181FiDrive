1f56bf51d286a2559dfc5f9dbe403a006ce373a2
NahirH <nadia.hernandez@est.fi.uncoma.edu.ar>
2020-09-04 15:30:37 -0300

    Datos añadidos

---
+++ README.md
@@ -0,0 +1,1 @@
+Trabajo de evaluación de avances para la materia Programación Web Dinámica, de la carrera "Tecnicatura Universitaria en Desarrollo Web" dictada por la Universidad Nacional del Comahue.
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo '<p>Esta es una aplicación para evaluar mis progresos en la materia PWD 2020.';
+echo '<p>Mi nombre es Nadia Nahir Hernandez</p>';
+echo '<p>Mi número de legajo es: FAI-2081</p>';
+
+
+?>
